61c88edb82452f4732a7e3b5aaf414ff9132e371
Dulli01 <luke.dalli.19@um.edu.mt>
2021-05-03 18:53:38 +0200

    added more service unit testing.

--- src/test/java/com/binarybeasts/sport/services/ClientEntityServiceTest.java
+++ src/test/java/com/binarybeasts/sport/services/ClientEntityServiceTest.java
@@ -4,6 +4,7 @@ import com.binarybeasts.sport.models.Client;
 import com.binarybeasts.sport.models.ClientEntity;
 import com.binarybeasts.sport.respositories.ClientRepository;
 import io.swagger.models.Model;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +12,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.tuple;
 import static org.mockito.Mockito.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -44,7 +48,8 @@ public class ClientEntityServiceTest {
 
         Optional<Client> savedClient = clientService.saveClient(clientToBeSaved);
 
-        assertThat(savedClient.get()).isEqualToComparingFieldByField(expectedSavedClient);
+        assertFalse(savedClient.isEmpty());
+        Assertions.assertThat(savedClient.get()).isEqualToComparingFieldByField(expectedSavedClient);
 
         verify(clientMockRepository, times(1)).save(any(ClientEntity.class));
     }
@@ -62,10 +67,76 @@ public class ClientEntityServiceTest {
 
         Optional<Client> savedClient = clientService.saveClient(clientToBeSaved);
 
-        assertThat(savedClient.get()).isEqualToComparingFieldByField(expectedSavedClient);
+        assertFalse(savedClient.isEmpty());
+        Assertions.assertThat(savedClient.get()).isEqualToComparingFieldByField(expectedSavedClient);
 
         verify(clientMockRepository, times(1)).save(any(ClientEntity.class));
     }
 
+    @Deprecated
+    @Test
+    void testGetUserByIdValidId() {
+        //Arrange
+        ClientEntity newClientEntity = new ClientEntity(1L,"123456L", "Joe Borg", 14, "joeborg@gmail.com", "79123456", true);
+        when(clientMockRepository.findById(1L)).thenReturn(Optional.of(newClientEntity));
+
+        Client expectedUser = modelMapper.map(newClientEntity, Client.class);
+
+        Optional<Client> actualClient = clientService.getClientById(1L);
+
+        //Assert
+        assertFalse(actualClient.isEmpty());
+        Assertions.assertThat(actualClient.get()).isEqualToComparingFieldByField(expectedUser);
+        verify(clientMockRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    void testGetUserByIdInvalidId() {
+        // Arrange
+        when(clientMockRepository.findById(1L)).thenReturn(Optional.empty());
+
+        // Act
+        Optional<Client> returnedUser = clientService.getClientById(1L);
+
+        // Assert
+        assertTrue(returnedUser.isEmpty());
+
+        verify(clientMockRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    void testDeleteUserByIdValidId() {
+        // Act
+        clientService.deleteClientById(1L);
+
+        // Assert
+        verify(clientMockRepository, times(1)).deleteById(1L);
+    }
+
+    @Deprecated
+    @Test
+    void testGetAllUsersValidUsers() {
+        //Arrange
+        List<ClientEntity> clientsList = List.of(
+                new ClientEntity("123456L", "Joe Borg", 14, "joeborg@gmail.com", "79123456", true),
+                new ClientEntity("123457L", "Mary Borg", 24, "maryborg@gmail.com", "79123457", true)
+        );
+
+        when(clientMockRepository.findAll()).thenReturn(clientsList);
+
+        Client expectedClient1 = new Client("123456L", "Joe Borg", 14, "joeborg@gmail.com", "79123456");
+        Client expectedClient2 = new Client("123457L", "Mary Borg", 24, "maryborg@gmail.com", "79123457");
+
+        List<Client> actualClientsList = clientService.getAllClients();
+
+        // Assert
+        assertThat(actualClientsList)
+                .extracting(Client::getId, Client::getFullName, Client::getAge, Client::getEmail, Client::getMobileNumber)
+                .containsExactly(
+                        tuple(expectedClient1.getId(), expectedClient1.getFullName(), expectedClient1.getAge(), expectedClient1.getEmail(), expectedClient1.getMobileNumber()),
+                        tuple(expectedClient2.getId(), expectedClient2.getFullName(), expectedClient2.getAge(), expectedClient2.getEmail(), expectedClient2.getMobileNumber())
+                );
+        verify(clientMockRepository, times(1)).findAll();
+    }
 
 }

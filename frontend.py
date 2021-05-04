fb404472891b7bc16f8857a7259dc1c38d1f110f
Luke Scerri <luke.scerri.19@um.edu.mt>
2021-05-04 19:02:56 +0200

    Updated front end

--- frontend.py
+++ frontend.py
@@ -1,7 +1,97 @@
 #python 3.8.5
 import requests
 
-#example api
-result = requests.get(url="http://dummy.restapiexample.com/api/v1/employees")
-jsondata = result.json()
-print(jsondata['status'])
+base_url = "http://127.0.0.1:8080/"
+
+def fullPath(ending):
+	return base_url + ending
+
+def post_req(ending, data):
+	headers = {
+		"Content-type": "application/json",
+		"Accept": "*/*"
+	}
+	result = requests.post(fullPath(ending), headers=headers, json=data)
+	return result.json()
+
+def get_req(url):
+	result = requests.get(url=fullPath(ending))
+	return result.json()
+
+main = """
+1.	Manage clients in the system
+2.	Manage bookings
+3.	Manage resources and property
+4.	Generate financial report
+5.	Exit"""
+
+def manageClients():
+	menu = """
+1. Add new client
+2. Remove client"""
+	print(menu)
+	num = int(input(">> "))
+
+def manageBookings():
+	menu = """
+1.	Schedlue new booking
+2.	Remove booking"""
+	print(menu)
+	
+	def schedule():
+		cid = input("client id >> ")
+		rid = input("resouce id >> ")
+		start = input("start time >> ")
+		duration = input("duration >> ")
+		data = {
+			"clientId": cid,
+			"length": duration,
+			"resourceId": rid,
+			"startTime": start
+		}
+		result = post_req("schedule", data)
+		if "error" in result:
+			print("Scheduling failed")
+			print(result["message"])
+		else:
+			print("Success")
+			print("Booking id: " + str(result["id"]))
+	
+	def remove():
+		bid = input("booking id >> ")
+		print("deleting " + str(bid))
+		#TODO: call delete api
+
+	bookingFuncs = {
+		1: schedule,
+		2: remove
+	}
+
+	num = int(input(">> "))
+	bookingFuncs[num]()
+
+def manageResources():
+	menu = """
+1. Add new resource
+2. Remove resource"""
+	print(menu)
+	num = int(input(">> "))
+
+def financialReport():
+	print("expenses: 500")
+	print("revenue: 0")
+
+mainFuncs = {
+	1:	manageClients,
+	2:	manageBookings,
+	3:	manageResources,
+	4:	financialReport
+}
+
+while True:
+	print(main)
+	num = int(input(">> "))
+	if num == 5:
+		print("Bye")
+		break
+	mainFuncs[num]()
